0c61dc02c1c538fe39af47498f8f3d83bb756019
peggy <173756665+peggy2125@users.noreply.github.com>
2024-09-11 20:54:37 +0800

    image size detection

--- odep_cellarray/pythoncode/截圖片座標.py
+++ odep_cellarray/pythoncode/截圖片座標.py
@@ -13,23 +13,46 @@ def main():
     # 加載圖片
     image = cv2.imread(filename)
     
+    cv2.namedWindow('Image Window', cv2.WINDOW_NORMAL)
+    cv2.imshow('Image Window', image)
+
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉所有窗口
+    
+    # 獲取原始圖像大小
     height, width, channels = image.shape
     print("原始圖像大小 (寬 x 高):", width, "x", height)
     
+    
     # 轉換為灰階圖
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray', gray)
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉所有窗口
     # 對圖像進行二值化處理
-    _, thresh = cv2.threshold(image, 10, 255, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY)
     
+    # 顯示二值化後的圖像
+    cv2.imshow('Binary Image', binary)
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉所有窗口
+
     # 找到輪廓
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     # 假設最大的輪廓是我們關心的物體
     largest_contour = max(contours, key=cv2.contourArea)
     
     # 獲取邊界矩形
     x, y, w, h = cv2.boundingRect(largest_contour)
+
+    # 創建新圖像 cropped
+    cropped = image[y:y+h, x:x+w].copy()
+    
+    # 顯示裁剪後的圖像
+    cv2.imshow('Cropped Image', cropped)
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉所有窗口
     
     # 計算四個角的座標
     top_left = (x, y)
@@ -39,14 +62,10 @@ def main():
     
     corners = [top_left, top_right, bottom_left, bottom_right]
     
-    # 檢查四個角是否形成正方形
-    side1 = np.linalg.norm(np.array(top_left) - np.array(top_right))
-    side2 = np.linalg.norm(np.array(top_left) - np.array(bottom_left))
     
-    is_square = np.isclose(side1, side2)
-    
+    # 輸出結果
     print("四角座標 (x, y):", corners)
-    print("是否為正方形:", is_square)
+    print("切割圖像大小:",(w,h))
 
 if __name__ == "__main__":
     main()

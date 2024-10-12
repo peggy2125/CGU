c0a9deddf1a0aaaa06068188032e99cab8e530a2
peggy2125 <173756665+peggy2125@users.noreply.github.com>
2024-10-13 06:22:46 +0800

    2024.10.13

--- old_anaconda_envs/labelme/Lib/site-packages/labelme/cli/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-from . import draw_json
-from . import draw_label_png
-from . import export_json
-from . import on_docker
--- old_anaconda_envs/labelme/Lib/site-packages/labelme/widgets/escapable_qlist_widget.py
+++
@@ -1,9 +0,0 @@
-from qtpy import QtWidgets
-from qtpy.QtCore import Qt
-
-
-class EscapableQListWidget(QtWidgets.QListWidget):
-    def keyPressEvent(self, event):
-        super(EscapableQListWidget, self).keyPressEvent(event)
-        if event.key() == Qt.Key_Escape:
-            self.clearSelection()

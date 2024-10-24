4946375f157e7c1a2135d76341e65c96854181cd
peggy2125 <173756665+peggy2125@users.noreply.github.com>
2024-10-24 15:43:03 +0800

    2024.10.24

--- odep_cellarray/pythoncode/all_p[articles_detect/canny.py
+++ odep_cellarray/pythoncode/all_p[articles_detect/canny.py
@@ -7,56 +7,38 @@ import os
 def process_image(image_path):
     # Load the image in grayscale
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    original_size = image.shape[1], image.shape[0]  # (width, height)
-
-    cv2.imshow('raw', image)
+    if image is None:
+        print(f"Failed to load image: {image_path}")
+    scale_percent = 50  # 缩放比例
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-
     start_time = time.time()
-
+    
     # Apply Gaussian blur to smooth the image
     blurred = cv2.GaussianBlur(image, (5, 5), 0)
-    blurred_resized = cv2.resize(blurred, original_size)
-    cv2.imshow('blurred', blurred_resized)
-
 
     # Apply threshold
-    _, binary = cv2.threshold(blurred, 90, 255, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(blurred, 75, 255, cv2.THRESH_BINARY)
     #binary = cv2.adaptiveThreshold(bg_sub, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 2)
-    cv2.imshow('binary', binary)
+       # 缩放图像
+    binary_resized = cv2.resize(binary, dim, interpolation=cv2.INTER_AREA)
 
-    # Erode and dilate to remove noise
-    #dilate1 = cv2.dilate(binary, kernel, iterations = 2)
-    #cv2.imshow('dilate1', dilate1)
-    #erode1 = cv2.erode(dilate1, kernel, iterations = 2)
-    #cv2.imshow('erode1', erode1)
-    #erode2 = cv2.erode(erode1, kernel, iterations = 1)
-    #cv2.imshow('erode2', erode2)
-    #dilate2 = cv2.dilate(erode2, kernel, iterations = 1)
-    #cv2.imshow('dilate2', dilate2)
+    # 显示缩放后的图像
+    cv2.imshow('resized_binary', binary_resized)
 
 
-
-    # Apply Canny edge detector to find edges
-    #edges = cv2.Canny(erode2, 50, 150)
-    #cv2.imshow('canny edges', edges)
-
-    # Trace contours from the edge image
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
  
     end_time = time.time()
     dif_time = end_time - start_time
     print(dif_time)
-
-    # Prepare an image to draw the contours
-    contour_image = np.zeros_like(image)
-
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    return binary
 
 # Set the directory containing your files
-directory = 'D:\\CGU\\odep_cellarray\\gray_whole_image_and_json'
+directory = "D:/CGU/odep_cellarray/gray_whole_rawimage_and_json"
 # # Get a list of all tiff files
 num=0
 files = [f for f in os.listdir(directory) if f.endswith('.png')]
@@ -64,4 +46,9 @@ for image in files:
     num+=1
     image_path = os.path.join(directory, image)
     print(num)
-    process_image(image_path)
+    binary=process_image(image_path)
+    base_filename = os.path.basename(image_path)  # 获取原文件名
+    output_folder = "D:\\CGU\\odep_cellarray\\binaryimage_and_json\\binary_threshold(75)"
+    output_path = os.path.join(output_folder, base_filename)  # 指定输出路径
+    cv2.imwrite(output_path, binary)  # 保存图像
+    print(f"Saved resized image to {output_path}")
